a6d5fcd68bf0bc61f2e4a9192ea026627047e842
Surik95 <pokemon.surik@gmail.com>
2022-10-08 19:50:30 +0400

    внес доработки

--- src/js/_test_/test.js
+++ src/js/_test_/test.js
@@ -1,16 +1,25 @@
 const { lifeIndication } = require('../index');
 
-test('Проверка работы функции healthy', () => {
-  const result = lifeIndication({ name: 'Маг', health: 90 });
-  expect(result).toBe('Маг healthy');
-});
+// test("Проверка работы функции healthy", () => {
+//   const result = lifeIndication({ name: "Маг", health: 90 });
+//   expect(result).toBe("Маг healthy");
+// });
 
-test('Проверка работы функции healthy', () => {
-  const result = lifeIndication({ name: 'Маг', health: 35 });
-  expect(result).toBe('Маг wounded');
-});
+// test("Проверка работы функции healthy", () => {
+//   const result = lifeIndication({ name: "Маг", health: 35 });
+//   expect(result).toBe("Маг wounded");
+// });
 
-test('Проверка работы функции healthy', () => {
-  const result = lifeIndication({ name: 'Маг', health: 10 });
-  expect(result).toBe('Маг critical');
+// test("Проверка работы функции healthy", () => {
+//   const result = lifeIndication({ name: "Маг", health: 10 });
+//   expect(result).toBe("Маг critical");
+// });
+
+test.each([
+  ['Маг healthy', { name: 'Маг', health: 90 }],
+  ['Маг wounded', { name: 'Маг', health: 35 }],
+  ['Маг critical', { name: 'Маг', health: 10 }],
+])('Проверка работы функции c показателем жизни %s', (expected, obj) => {
+  const result = lifeIndication(obj);
+  expect(result).toBe(expected);
 });
